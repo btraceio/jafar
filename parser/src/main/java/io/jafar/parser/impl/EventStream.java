9bbdcccddb6bdc993092e427d4942d9ba036caa8
Jaroslav Bachorik <j.bachorik@gmail.com>
2025-09-28 22:16:06 +0200

    Lazy cp maps for untyped parser

--- parser/src/main/java/io/jafar/parser/impl/EventStream.java
+++ parser/src/main/java/io/jafar/parser/impl/EventStream.java
@@ -5,14 +5,11 @@ import io.jafar.parser.api.ParserContext;
 import io.jafar.parser.internal_api.CheckpointEvent;
 import io.jafar.parser.internal_api.ChunkHeader;
 import io.jafar.parser.internal_api.ChunkParserListener;
-import io.jafar.parser.internal_api.ConstantPoolValueProcessor;
 import io.jafar.parser.internal_api.GenericValueReader;
 import io.jafar.parser.internal_api.RecordingStream;
-import io.jafar.parser.internal_api.ValueProcessor;
 import io.jafar.parser.internal_api.metadata.MetadataClass;
 import io.jafar.parser.internal_api.metadata.MetadataEvent;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -46,226 +43,18 @@ public abstract class EventStream implements ChunkParserListener {
 
   @Override
   public final boolean onChunkStart(ParserContext context, int chunkIndex, ChunkHeader header) {
-    // store a new MutableConstantPools
-    context.put(ConstantPools.class, new ConstantPools());
-
     ((ControlImpl) control.get()).setStream(context.get(RecordingStream.class));
     context.put(Control.ChunkInfo.class, new ChunkInfoImpl(header));
 
-    // ! these two keys should be removed at `onChunkEnd` to avoid any unwanted leakage
-
-    // register a custom value processor which can also work with constant pools
-    // the processor instance is exclusive for the chunk - chunks can be processed in parallel so be
-    // prepared
-    // the callbacks are executed as follows:
-    // - onConstantPoolValueStart -> <field processing> -> onConstantPoolValueEmd
-    // field processing can be either:
-    // - array: onArrayStart -> onValue | onConConstantPoolIndex -> onArrayEnd
-    // - inline complex type: onComplexValueStart -> <field processing> -> onComplexValueEnd
-    // - constant pool index: onConstantPoolIndex
-    // - primitive value: on*Value
-    ConstantPoolValueProcessor cpValueProcessor =
-        new ConstantPoolValueProcessor() {
-          private final MultiTypeStack stack = new MultiTypeStack(20);
-
-          @Override
-          public void onConstantPoolValueStart(MetadataClass type, long id) {
-            Map<String, Object> value = new HashMap<>();
-            stack.push(value);
-          }
-
-          @SuppressWarnings("unchecked")
-          @Override
-          public void onConstantPoolValueEnd(MetadataClass type, long id) {
-            Map<String, Object> value = (Map<String, Object>) stack.pop(Map.class);
-            assert value != null && !value.isEmpty();
-            ParserContext ctx = type.getContext();
-            ConstantPools cpools = ctx.get(ConstantPools.class);
-            assert cpools != null;
-            cpools.add(type.getId(), id, value);
-          }
-
-          @Override
-          public void onStringValue(MetadataClass owner, String fld, String value) {
-            ArrayHolder ah = stack.peek(ArrayHolder.class);
-            if (ah != null) {
-              ah.add(value);
-            } else {
-              Map<String, Object> parent = (Map<String, Object>) stack.peek(Map.class);
-              assert parent != null;
-              parent.put(fld, value);
-            }
-          }
-
-          @SuppressWarnings("unchecked")
-          @Override
-          public void onComplexValueStart(MetadataClass owner, String fld, MetadataClass type) {
-            Map<String, Object> value = new HashMap<>();
-            stack.push(value);
-          }
-
-          @Override
-          public void onShortValue(MetadataClass type, String fld, short value) {
-            ArrayHolder ah = stack.peek(ArrayHolder.class);
-            if (ah != null) {
-              ah.add(value);
-            } else {
-              Map<String, Object> parent = (Map<String, Object>) stack.peek(Map.class);
-              assert parent != null;
-              parent.put(fld, value);
-            }
-          }
-
-          @Override
-          public void onCharValue(MetadataClass type, String fld, char value) {
-            ArrayHolder ah = stack.peek(ArrayHolder.class);
-            if (ah != null) {
-              ah.add(value);
-            } else {
-              Map<String, Object> parent = (Map<String, Object>) stack.peek(Map.class);
-              assert parent != null;
-              parent.put(fld, value);
-            }
-          }
-
-          @Override
-          public void onIntValue(MetadataClass owner, String fld, long value) {
-            ArrayHolder ah = stack.peek(ArrayHolder.class);
-            if (ah != null) {
-              ah.add(value);
-            } else {
-              Map<String, Object> parent = (Map<String, Object>) stack.peek(Map.class);
-              assert parent != null;
-              parent.put(fld, value);
-            }
-          }
-
-          @Override
-          public void onLongValue(MetadataClass type, String fld, long value) {
-            ArrayHolder ah = stack.peek(ArrayHolder.class);
-            if (ah != null) {
-              ah.add(value);
-            } else {
-              Map<String, Object> parent = (Map<String, Object>) stack.peek(Map.class);
-              assert parent != null;
-              parent.put(fld, value);
-            }
-          }
-
-          @Override
-          public void onByteValue(MetadataClass type, String fld, byte value) {
-            ArrayHolder ah = stack.peek(ArrayHolder.class);
-            if (ah != null) {
-              ah.add(value);
-            } else {
-              Map<String, Object> parent = (Map<String, Object>) stack.peek(Map.class);
-              assert parent != null;
-              parent.put(fld, value);
-            }
-          }
-
-          @Override
-          public void onBooleanValue(MetadataClass owner, String fld, boolean value) {
-            ArrayHolder ah = stack.peek(ArrayHolder.class);
-            if (ah != null) {
-              ah.add(value);
-            } else {
-              Map<String, Object> parent = (Map<String, Object>) stack.peek(Map.class);
-              assert parent != null;
-              parent.put(fld, value);
-            }
-          }
-
-          @Override
-          public void onDoubleValue(MetadataClass owner, String fld, double value) {
-            ArrayHolder ah = stack.peek(ArrayHolder.class);
-            if (ah != null) {
-              ah.add(value);
-            } else {
-              Map<String, Object> parent = (Map<String, Object>) stack.peek(Map.class);
-              assert parent != null;
-              parent.put(fld, value);
-            }
-          }
-
-          @Override
-          public void onFloatValue(MetadataClass owner, String fld, float value) {
-            ArrayHolder ah = stack.peek(ArrayHolder.class);
-            if (ah != null) {
-              ah.add(value);
-            } else {
-              Map<String, Object> parent = (Map<String, Object>) stack.peek(Map.class);
-              assert parent != null;
-              parent.put(fld, value);
-            }
-          }
-
-          @Override
-          public void onComplexValueEnd(MetadataClass owner, String fld, MetadataClass type) {
-            Map<String, Object> value = stack.pop(Map.class);
-            assert value != null;
-            ArrayHolder ah = stack.peek(ArrayHolder.class);
-            if (ah != null) {
-              ah.add(value);
-            } else {
-              if (owner != null && fld != null) {
-                Map<String, Object> parent = stack.peek(Map.class);
-                if (parent != null) {
-                  parent.put(fld, value);
-                }
-              } else {
-                // a special case of 'free-floating' value - will be made available in the context
-                //    as 'type-name#value' of type Map
-                context.put(type.getName() + "#value", Map.class, value);
-              }
-            }
-          }
-
-          @Override
-          public void onArrayStart(MetadataClass owner, String fld, MetadataClass type, int len) {
-            ArrayHolder arr = new ArrayHolder(type.getName(), len);
-            stack.push(arr);
-          }
-
-          @Override
-          public void onArrayEnd(MetadataClass owner, String fld, MetadataClass type) {
-            ArrayHolder arr = stack.pop(ArrayHolder.class);
-            assert arr != null;
-            Map<String, Object> parent = stack.peek(Map.class);
-            assert parent != null;
-            parent.put(fld, arr);
-          }
-
-          @Override
-          public void onConstantPoolIndex(
-              MetadataClass owner, String fld, MetadataClass type, long pointer) {
-            ParserContext ctx = owner.getContext();
-            assert ctx != null;
-            ConstantPools cpools = ctx.get(ConstantPools.class);
-            ConstantPoolAccessor cpAccessor = new ConstantPoolAccessor(cpools, type, pointer);
-
-            ArrayHolder ah = stack.peek(ArrayHolder.class);
-            if (ah != null) {
-              ah.add(cpAccessor);
-            } else {
-              Map<String, Object> parent = stack.peek(Map.class);
-              assert parent != null;
-              parent.put(fld, cpAccessor);
-            }
-          }
-        };
-    context.put(ConstantPoolValueProcessor.class, cpValueProcessor);
-    // store also the value reader instance to be reused in onEvent
-    context.put(GenericValueReader.class, new GenericValueReader(cpValueProcessor));
+    // register a Map-building value processor backed by lazy constant-pool accessors.
+    // No need to store a processor/reader in the context for constants;
+    // we build event maps per-event and resolve constants lazily.
     return delegate == null || delegate.onChunkStart(context, chunkIndex, header);
   }
 
   @Override
   public final boolean onChunkEnd(ParserContext context, int chunkIndex, boolean skipped) {
-    //        context.remove(ConstantPools.class); // remove the constant pools from context
-    // also clean up the parsers
-    context.remove(ConstantPoolValueProcessor.class);
-    context.remove(GenericValueReader.class);
+    // nothing to clean up here
 
     return delegate == null || delegate.onChunkEnd(context, chunkIndex, skipped);
   }
@@ -276,17 +65,17 @@ public abstract class EventStream implements ChunkParserListener {
       ParserContext context, long typeId, long eventStartPos, long rawSize, long payloadSize) {
     ControlImpl ctl = (ControlImpl) control.get();
     try {
-      GenericValueReader r = context.get(GenericValueReader.class);
-      ValueProcessor vp = context.get(ConstantPoolValueProcessor.class);
+      MapValueBuilder builder = new MapValueBuilder(context);
+      GenericValueReader r = new GenericValueReader(builder);
 
       MetadataClass eventClz = context.getMetadataLookup().getClass(typeId);
       try {
-        vp.onComplexValueStart(null, null, eventClz);
+        builder.reset();
+        builder.onComplexValueStart(null, null, eventClz);
         r.readValue(context.get(RecordingStream.class), eventClz);
       } finally {
-        vp.onComplexValueEnd(null, null, eventClz);
-        Map<String, Object> value =
-            (Map<String, Object>) context.remove(eventClz.getName() + "#value", Map.class);
+        builder.onComplexValueEnd(null, null, eventClz);
+        Map<String, Object> value = builder.getRoot();
 
         // Process event value with parsed data
         onEventValue(eventClz, value, ctl);
